1e0cd68e2fe960e2028e1dd295b0e457290ddeba
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-25 00:18:12 -0600

    Added score tracking and writing to scoreboard

--- pong.py
+++ pong.py
@@ -7,6 +7,10 @@ wn.setup(width=800, height=600)
 wn.tracer(0)
 
 
+# Score
+score_a = 0
+score_b = 0
+
 #Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -101,10 +105,17 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     # Paddle collisions
 

f6aa081b0a7a88bdebe138e128d185e0d397469c
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-24 23:44:15 -0600

    Full paddle collisions for left and right sides

--- pong.py
+++ pong.py
@@ -99,5 +99,12 @@ while True:
         ball.goto(0, 0)
         ball.dx *= -1
 
-    if ball.xcor() > 340 and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
+    # Paddle collisions
+
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40):
+        ball.setx(-340)
         ball.dx *= -1

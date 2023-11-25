c76625d8032cdb2e63dafaa5aa75c4866db1444c
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-24 23:33:41 -0600

    Added bounce on right paddle

--- pong.py
+++ pong.py
@@ -98,3 +98,6 @@ while True:
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+
+    if ball.xcor() > 340 and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
+        ball.dx *= -1

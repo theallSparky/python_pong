02be5dff461b4809dc74ecce4283ac56dcc0ca00
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-25 10:33:11 -0600

    Added bounce sound and its triggers

--- pong.py
+++ pong.py
@@ -1,4 +1,5 @@
 import turtle
+import os
 
 wn = turtle.Screen()
 wn.title("Pong by the_allSparky")
@@ -97,10 +98,13 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        os.system('afplay bounce.wav&')
 
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+        os.system('afplay bounce.wav&')
+
     
     if ball.xcor() > 390:
         ball.goto(0, 0)
@@ -122,7 +126,10 @@ while True:
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
         ball.setx(340)
         ball.dx *= -1
+        os.system('afplay bounce.wav&')
+
 
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40):
         ball.setx(-340)
         ball.dx *= -1
+        os.system('afplay bounce.wav&')

ebca1a2887b26dcb500833cd72cf8698439b2774
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-24 21:04:44 -0600

    Made paddle b

--- pong.py
+++ pong.py
@@ -19,8 +19,14 @@ paddle_a.goto(-350, 0)
 
 
 
-#Paddle A
-
+#Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
 
 
 
